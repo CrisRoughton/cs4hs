98514483ed088cffe6cde5fee536da8ffa9c95bf
Cris CrisRoughton <cris.roughton@wegc.school.nz>
2018-11-27 13:54:01 +1300

    added 4th line in for prac

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello Edwin, you must have more patience")
 print("Patience is a virtue!")
 print("how do I get it into staged again?")
+print("could be prompted for username or password...")
